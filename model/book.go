81197b8f1af0c5c208333b2f0cbd98d0e7b795ae
thotsaphonjantree <thotsaphon.jantree@gmail.com>
2019-11-21 14:07:24 +0700

    add tags in book model

--- model/book.go
+++ model/book.go
@@ -4,11 +4,11 @@ import "gopkg.in/mgo.v2/bson"
 
 // Book Model
 type Book struct {
-	BookID     bson.ObjectId `json:"_id" bson:"_id"`
-	Isbn       string        `json:"isbn" bson:"isbn"`
-	Title      string        `json:"title" bson:"title"`
-	Authors    []string      `json:"authors" bson:"authors"`
-	Publishers string        `json:"publishers" bson:"publishers"`
-	ImgURL     string        `json:"imgURL" bson:"imgURL"`
-	//Tags       []bson.ObjectId `json:"tags" bson:"tags"`
+	BookID     bson.ObjectId    `json:"_id" bson:"_id"`
+	Isbn       string           `json:"isbn" bson:"isbn"`
+	Title      string           `json:"title" bson:"title"`
+	Authors    []string         `json:"authors" bson:"authors"`
+	Publishers string           `json:"publishers" bson:"publishers"`
+	ImgURL     string           `json:"imgURL" bson:"imgURL"`
+	Tags       []*bson.ObjectId `json:"tags" bson:"tags"`
 }
